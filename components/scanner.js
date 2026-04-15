4dd275ccdaf28ce5df01645080d706e456f9f5aa
felixid <bp084318@gmail.com>
2026-04-15 12:05:25 +0700

    Delete components directory

--- components/scanner.js
+++
@@ -1,9 +0,0 @@
-export function calculateRisk(data) {
-  let risk = 0;
-
-  if (data.location) risk += 30;
-  if (data.phone) risk += 40;
-  if (data.sameuser) risk += 20;
-
-  return risk;
-}
--- components/ui.js
+++
@@ -1,9 +0,0 @@
-export function showResult(score) {
-  let status = "AMAN";
-
-  if (score >= 70) status = "BAHAYA";
-  else if (score >= 40) status = "WASPADA";
-
-  document.getElementById("status").innerText = status;
-  document.getElementById("score").innerText = "Risk: " + score;
-}
